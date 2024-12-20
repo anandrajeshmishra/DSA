70ea9943362149eb4fc3eca6006253987836d4cf
anandrajeshmishra <anand98425@gmail.com>
2024-12-20 17:44:35 +0530

    work

---
+++ DAY14.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int decimalToBinary( int num){
+    int binary = 0;
+    int remainder;
+    int pow = 1;
+    while (num>0)
+{
+    remainder = num %2;
+    num = num/2;
+    binary = binary + (remainder*pow);
+    pow = pow*10;
+}
+return binary;
+}
+int main(){
+int num;
+cout<<"Enter the number to convert to binary : ";
+cout<<endl;
+cin>>num;
+cout<<"The converted number is "<<decimalToBinary(num);
+return 0;
+}
