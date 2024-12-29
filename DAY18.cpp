673726c7a585866dd61617f8c091b977ea3aaa0a
anandrajeshmishra <anand98425@gmail.com>
2024-12-29 15:08:32 +0530

    work

---
+++ DAY18.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main(){
+// vector <int> nums = {1,2,3,1};
+// int ans = 0, freq = 0;
+// for (int i = 0; i < nums.size(); i++)
+// {
+//     if (freq==0)
+//     {
+//         ans = nums[i];
+//     }if (ans == nums[i])
+//     {
+//         i++;
+//     }else{
+//         i--;
+//     }
+// }
+// cout<<ans;
+
+// return 0;
+// }
+#include <iostream>
+using namespace std;
+int main(){
+
+return 0;
+}
