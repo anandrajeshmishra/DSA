f598499d2eee735459781b6e08b2b06d95c941a6
anandrajeshmishra <anand98425@gmail.com>
2025-01-17 16:35:58 +0530

    work

--- DAY29.cpp
+++ DAY29.cpp
@@ -55,8 +55,91 @@
 // return 0;
 // }
 #include <iostream>
+#include <algorithm>
 using namespace std;
+int maximumRowSum(int matrix[4][3], int rows, int colums){
+    int largest = INT16_MIN;
+    for (int i = 0; i < rows; i++)
+    {   
+        int rowsum =0;
+        for (int j = 0; j < colums; j++)
+        {  
+            rowsum += matrix[i][j];            
+        }
+        largest = max(largest,rowsum);
+        
+    }
+    return largest;
+    
+}
+int diagonalSum(int matrix[3][3], int rows, int colums){
+    int sum = 0;
+    // for (int i = 0; i < rows; i++)
+    // {
+    //     for (int j = 0; j < rows; j++)
+    //     {
+    //         if (i==j)
+    //         {
+    //             sum+=matrix[i][j];
+    //         }else if(j == rows-i-1){
+    //                 sum+=matrix[i][j];
+    //         }
+            
+    //     }
+        
+    // }
+    for (int i = 0; i < rows; i++)
+    {
+        sum+=matrix[i][i];
+        if (i!=rows-i-1)
+        {
+            sum+=matrix[i][rows-i-1];
+        }
+        
+    }
+    
+    return sum;
+}
+bool linearSearch(int matrix[4][3], int rows, int colums, int key){
+    for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < colums; j++)
+    {
+        if (matrix[i][j] == key)
+        {
+            return true;
+        }
+        
+    }
+}
+return false;
+}
 int main(){
-
+int matrix [3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+int rows = 3;
+int colums = 3;
+int key = 8;
+cout<<diagonalSum(matrix,rows,colums);
+// cout<<maximumRowSum(matrix, rows,colums);
+// cout<<linearSearch(matrix,rows,colums,key);
+// // cout<<matrix[2][1]<<endl;
+// //input
+// for (int i = 0; i < rows; i++)
+// {
+//     for (int j = 0; j < colums; j++)
+//     {
+//         cin>>matrix[i][j];
+//     }
+// }
+// //output
+// for (int i = 0; i < rows; i++)
+// {
+//     for (int j = 0; j < colums; j++)
+//     {
+//         cout<<matrix[i][j]<<" ";
+//     }
+//     cout<<endl;
+// }
+// cout<<endl;
 return 0;
 }
