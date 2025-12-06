db4761e0dc59a9ac61ffe98ed1764b9d6b18ff81
anandrajeshmishra <anand98425@gmail.com>
2025-12-06 12:43:41 +0530

    work

---
+++ DAY33.cpp
@@ -0,0 +1,31 @@
+//To print the square number pattern
+// #include <iostream>
+// using namespace std;
+// int main(){
+// int n;
+// cout<<"Enter any number to see the pattern";
+// cin>>n;
+// for(int i = 1; i<=n; i++){
+//     for(int j = 1; j<=n; j++){
+//         cout<<j;
+//     }
+//     cout<<endl;
+// }
+// return 0;
+// }
+
+//To print the square * pattern
+// #include <iostream>
+// using namespace std;
+// int main(){int n;
+//     cout<<"Enter the number to which you have to print the star";
+//     cin>>n;
+//     for(int i =1; i<=n; i++){
+//         for(int j=1; j<=n; j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+// 
