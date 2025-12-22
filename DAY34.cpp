b45202d02d304ebee997404c9dd8ba33f9241bd8
anandrajeshmishra <anand98425@gmail.com>
2025-12-22 12:44:20 +0530

    work

---
+++ DAY34.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+    if(n>0){
+        cout<<"The value of n is POSITIVE"<<endl;
+    }else{
+        cout<<"The value of n is NEGATIVE";
+    }
+    return 0;
+}
