9d1408844d4ad55ef4c2be7f03aea25e5502ee1a
anandrajeshmishra <anand98425@gmail.com>
2025-12-22 12:57:38 +0530

    work

--- DAY34.cpp
+++ DAY34.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<<"Enter the value of n"<<endl;
-    cin>>n;
-    if(n>0){
-        cout<<"The value of n is POSITIVE"<<endl;
+    // int n;
+    // cout<<"Enter the value of n"<<endl;
+    // cin>>n;
+    // if(n>0){
+    //     cout<<"The value of n is POSITIVE"<<endl;
+    // }else{
+    //     cout<<"The value of n is NEGATIVE";
+    // }
+    // return 0;
+    // int a,b;
+    // cout<<"Enter the value of a and b";
+    // cin>>a>>b;
+    // if(a>b){
+    //     cout<<"A is Greater than B";
+    // }else{
+    //     cout<<"B is Greater than A";
+    // }
+    int a;
+    cout<<"Enter the value of a and b ";
+    cin>>a;
+    if(a>0){
+        cout<<"The value of a is positive"<<endl;
     }else{
-        cout<<"The value of n is NEGATIVE";
-    }
+        if(a<0){
+            cout<<"The value of a is negative"<<endl;
+        }else{
+            cout<<"The value of a is 0"<<endl;
+        }
+}
     return 0;
 }
