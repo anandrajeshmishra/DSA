9ac25d1b4c9b2c75abc9ada5857d13d2f13ebbd1
anandrajeshmishra <anand98425@gmail.com>
2025-12-23 13:16:35 +0530

    work

--- DAY34.cpp
+++ DAY34.cpp
@@ -306,18 +306,36 @@ int main(){
 // BB
 // CCC
 // DDDD
-int n;
-cin>>n;
-int row = 1;
-while(row<=n){
-    int col = 1;
-    while(col<=row){
-        char ch = 'A'+row-1;
-        cout<<ch;
-        col++;
-    }
-    cout<<endl;
-    row++;
-}
+// int n;
+// cin>>n;
+// int row = 1;
+// while(row<=n){
+//     int col = 1;
+//     while(col<=row){
+//         char ch = 'A'+row-1;
+//         cout<<ch;
+//         col++;
+//     }
+//     cout<<endl;
+//     row++;
+// }
+// A
+// BC
+// DEF
+// GHIJ
+// int n;
+// cin>>n;
+// int row = 1;
+// char value = 'A';
+// while(row<=n){
+//     int col = 1;
+//     while(col<=row){
+//         cout<<value;
+//         value++;
+//         col++;
+//     }
+//     cout<<endl;
+//     row++;
+// }
 return 0;
 }
