fe9a7aa3e1d69236741b9468407402a9ed219be0
anandrajeshmishra <anand98425@gmail.com>
2025-12-24 12:05:12 +0530

    work

--- DAY35.cpp
+++ DAY35.cpp
@@ -9,12 +9,40 @@ int main(){
 // cout<<"a^b  "<<(a^b)<<endl;
 // cout<<(16>>2)<<endl;
 // cout<<(16<<2);
-int n;
-cin>>n;
-int sum = 0;
-for(int i = 1; i<=n; i++){
-    sum+=i;
-}
-cout<<sum;
+// int n;
+// cin>>n;
+// int sum = 0;
+// for(int i = 1; i<=n; i++){
+//     sum+=i;
+// }
+// cout<<sum;
+// fibonacci numbers
+// int n;
+// cin>>n;
+// int a = 0;
+// int b = 1;
+// cout<<a<<" "<<b<<" ";
+// for(int i = 0; i<=n; i++){
+//     int nextNumber = a+b;
+//     cout<<nextNumber<<" ";
+//     a = b;
+//     b = nextNumber;
+// }
+// int n;
+// cout<<"Enter a number to find out that the given number is prime or not :";
+// cin>>n;
+// cout<<endl;
+// bool isprime = 1;
+// for(int i = 2; i<n; i++){
+//     if(n%i==0){
+//         isprime = 0;
+//         break;
+//     }
+// }
+// if(isprime==0){
+//     cout<<"The given number is not a prime number"<<endl;
+// }else{
+//     cout<<"The given number is a prime number";
+// }
 return 0;
 }
